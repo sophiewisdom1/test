24856c5f03422e10fb21357648362d090f77a6e8
sophiewisdom1 <123639925+sophiewisdom1@users.noreply.github.com>
2023-01-26 12:20:18 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+//Распознавание устройства
+let isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+
+
+//Меню-бургер
+const iconMenu = document.querySelector('.header__burger');
+const menuHeader = document.querySelector('.header__menu');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuHeader.classList.toggle('_active');
+	});
+}
